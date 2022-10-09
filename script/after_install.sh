a5316deb2dd1cb7f25bcad83d79431653d7aa6d6
vo.van.quang <vo.van.quang@sun-asterisk.com>
2022-10-09 12:02:22 +0700

    update script

--- script/after_install.sh
+++ script/after_install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+set -e
+set -x
+
 RED='\033[0;31m'
 NC='\033[0m'
 RELEASE_FOLDER=$(date '+%Y%m%d%H%M%S')
 
-# cd /home/deploy/blog-cloud
-
-# echo -e "${RED}execute ls"
+echo -e "${RED}After Install"
 # echo $(ls)
 
 # 1 update shared file, folder
 if ! [ -d /var/www/rails_app/shared ]; then
   mkdir -p /var/www/rails_app/shared
   mkdir -p /var/www/rails_app/shared/bundle
+  mkdir -p /var/www/rails_app/shared/log
+  mkdir -p /var/www/rails_app/shared/config
 fi
 
 aws s3 cp s3://blog-cloud-codedeploy/.env /var/www/rails_app/shared
-aws s3 cp s3://blog-cloud-codedeploy/credentials.yml.enc /var/www/rails_app/shared
-aws s3 cp s3://blog-cloud-codedeploy/puma.rb /var/www/rails_app/shared
-
+aws s3 cp s3://blog-cloud-codedeploy/credentials.yml.enc /var/www/rails_app/shared/config
+aws s3 cp s3://blog-cloud-codedeploy/puma.rb /var/www/rails_app/shared/config
 
 # 2 Create release folder
 
@@ -28,36 +30,33 @@ cp -R /home/deploy/blog-cloud/* /var/www/rails_app/releases/$RELEASE_FOLDER
 
 # 3
 if [ -d /var/www/rails_app/releases/$RELEASE_FOLDER/log ]; then
-  rm -rf /var/www/rails_app/releases/$RELEASE_FOLDER/log/*
+  rm -rf /var/www/rails_app/releases/$RELEASE_FOLDER/log
 fi
 
 if [ -f /var/www/rails_app/releases/$RELEASE_FOLDER/config/credentials.yml.enc ]; then
   rm /var/www/rails_app/releases/$RELEASE_FOLDER/config/credentials.yml.enc
-end
+fi
 
 if [ -f /var/www/rails_app/releases/$RELEASE_FOLDER/config/puma.rb ]; then
   rm /var/www/rails_app/releases/$RELEASE_FOLDER/config/puma.rb
-end
+fi
 
 ln -s /var/www/rails_app/shared/.env /var/www/rails_app/releases/$RELEASE_FOLDER/
-
-ln -s /var/www/rails_app/shared/credentials.yml.enc /var/www/rails_app/releases/$RELEASE_FOLDER/config
-
-ln -s /var/www/rails_app/shared/puma.rb /var/www/rails_app/releases/$RELEASE_FOLDER/config
-
 ln -s /var/www/rails_app/shared/bundle /var/www/rails_app/releases/$RELEASE_FOLDER/vendor/bundle
-
+ln -s /var/www/rails_app/shared/config/credentials.yml.enc /var/www/rails_app/releases/$RELEASE_FOLDER/config
+ln -s /var/www/rails_app/shared/config/puma.rb /var/www/rails_app/releases/$RELEASE_FOLDER/config
+ln -s /var/www/rails_app/shared/log /var/www/rails_app/releases/$RELEASE_FOLDER/log
 
 # 4 bundle
-
-bundle install --path=vendor/bundle
-bundle exec rake db:create
-bundle exec rake db:migrate
+cd /var/www/rails_app/releases/$RELEASE_FOLDER
+/home/deploy/.rbenv/shims/bundle install --path=vendor/bundle
+/home/deploy/.rbenv/shims/bundle exec rake db:create
+/home/deploy/.rbenv/shims/bundle exec rake db:migrate
 
 # 5 Link to shared folder
 
 ln -s /var/www/rails_app/releases/$RELEASE_FOLDER /var/www/rails_app/releases/current
-mv /var/www/rails_app/releases/current /var/www/rails_app/
+mv -f /var/www/rails_app/releases/current /var/www/rails_app/
 
 # restart nginx
 sudo service puma restart
--- script/before_install.sh
+++ script/before_install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+RED='\033[0;31m'
+
+echo -e "${RED}======Before Install=========="
 
 if [ -d /home/deploy/blog-cloud ]; then
   rm -rf /home/deploy/blog-cloud
 fi
 
 mkdir -vp /home/deploy/blog-cloud
+
+echo -e "${RED}======End of Before Install===="
