6d25b5754db794c37d9909e2cb0bd67fd0cd728e
vo.van.quang <vo.van.quang@sun-asterisk.com>
2022-10-09 16:26:15 +0700

    test cd

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,5 +1,5 @@
 class HomePagesController < ApplicationController
   def index
-    render json: "Welcome to my page edit at #{Time.current}", status: 200
+    render json: "Update code #{Time.current}", status: 200
   end
 end
